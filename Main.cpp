595c0ee646182126c37fe9a7274eb14f4e2f1137
Chanira <chanirapannala@gmail.com>
2024-03-30 09:40:08 +0530

    feat: Create window

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,69 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include<iostream> // input/output operations
+#include<glad/glad.h> //GLAD library which manages function pointers for OpenGL
+#include<GLFW/glfw3.h> // includes the GLFW library for creating windows and handling input.
 
 int main()
 {
+	// Initialize GLFW
+	glfwInit(); // initializes the GLFW library. This function must be called before any other GLFW functions are used.
+
+
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Youtube", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);// the newly created window is set as the current OpenGL context.
+
+
+
+	//Load GLAD so it configures OpenGL
+	//loads the OpenGL function pointers using the GLAD library
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+
+
+
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+
+
+	// Main while loop
+	// It continues running until the window should close
+	while (!glfwWindowShouldClose(window))
+	{
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
 	return 0;
 }
